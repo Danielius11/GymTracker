327459fd19cc43196277d00af1579a8514fc638e
Danielius Miliunec <danielius.miliunec@vilniustech.lt>
2025-04-29 20:38:26 +0300

    Created Day model with basic workout properties

---
+++ Gym/Models/Day.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gym.Models
+{
+    public class Day
+    {
+        public int Nr { get; set; }
+        public string Type { get; set; }
+        public string Exercise { get; set; }
+        public int Set { get; set; }
+        public int Rep { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+    }
+}
