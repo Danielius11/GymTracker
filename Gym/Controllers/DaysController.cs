037eee06b4a9c952d8c335307c9896b480f5836d
Danielius Miliunec <danielius.miliunec@vilniustech.lt>
2025-05-15 19:07:57 +0300

    Added Edit and Delete options

--- Gym/Controllers/DaysController.cs
+++ Gym/Controllers/DaysController.cs
@@ -3,6 +3,7 @@ using Gym.Models;
 using Gym.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace Gym.Controllers
 {
@@ -48,6 +49,51 @@ namespace Gym.Controllers
             var days = await dbcontext.Days.ToListAsync();
 
             return View(days);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var day = await dbcontext.Days.FindAsync(id);
+
+            return View(day);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Day viewmodel)
+        {
+            var day = await dbcontext.Days.FindAsync(viewmodel.Id);
+
+            if (day is not null)
+            {
+                day.Nr = viewmodel.Nr;
+                day.Type = viewmodel.Type;
+                day.Exercise = viewmodel.Exercise;
+                day.Set = viewmodel.Set;
+                day.Rep = viewmodel.Rep;
+                day.Kg = viewmodel.Kg;
+                day.Description = viewmodel.Description;
+
+                await dbcontext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Days");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Day viewmodel)
+        {
+            var day = await dbcontext.Days
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == viewmodel.Id);
+
+            if (day is not null)
+            {
+                dbcontext.Days.Remove(viewmodel);
+                await dbcontext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Days");
         }
     }
 }
